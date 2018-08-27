1818846415390b9cfa34cb35a15dbaa73da8749e
Goutam Hegde <goutam.hegde@iteego.com>
2018-08-27 18:59:45 +0530

    Quickfix

---
+++ quickfixj/bin/executor.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Scripts for the QFJ binary distribution
+
+scriptdir=`dirname $0`
+qfjhome=$scriptdir/..
+
+CP=$qfjhome/lib/mina-core-1.1.0.jar:$qfjhome/lib/slf4j-api-1.3.0.jar:$qfjhome/lib/slf4j-jdk14-1.3.0.jar:$qfjhome/quickfixj-all-1.3.3.jar:$qfjhome/quickfixj-core-1.3.3.jar:$qfjhome/quickfixj-msg-fix40-1.3.3.jar:$qfjhome/quickfixj-msg-fix41-1.3.3.jar:$qfjhome/quickfixj-msg-fix42-1.3.3.jar:$qfjhome/quickfixj-msg-fix43-1.3.3.jar:$qfjhome/quickfixj-msg-fix44-1.3.3.jar:$qfjhome/quickfixj-examples-1.3.3.jar
+
+java -classpath "$CP"  quickfix.examples.executor.Executor $*
